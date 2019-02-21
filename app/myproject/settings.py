8c82210ca09b16e96079aee3c20aa03c72ff3f37
ShouyaToyama <shouya.toyama@gmail.com>
2019-02-21 21:02:27 +0900

    Modify debug flag in Django

--- app/myproject/settings.py
+++ app/myproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANG_DEBUG')
+DEBUG = os.getenv('DJANGO_DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
