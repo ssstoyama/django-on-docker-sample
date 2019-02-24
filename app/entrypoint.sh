ed13566a1fbe77a28fa539cf3faf74370347b668
ShouyaToyama <shouya.toyama@gmail.com>
2019-02-24 22:08:44 +0900

    Modify user in Django container

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -16,6 +16,5 @@ python manage.py flush --no-input
 python manage.py makemigrations myapp
 python manage.py migrate
 python manage.py loaddata mymodel
-python manage.py collectstatic --no-input
 
 exec "$@"
